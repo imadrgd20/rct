6e0af6fee95abaa75d026c4d89599d74bd0cd6fc
imadrgd20 <reguadiimad@gmail.com>
2024-05-31 17:31:54 +0100

    slksks

---
+++ NodeServer/server.js
@@ -0,0 +1,32 @@
+
+const express = require('express');
+const app = express();
+
+app.get('/api/timestamp/:dateString?', (req, res) => {
+    let dateString = req.params.dateString;
+    
+    // If dateString is undefined, use the current date
+    if (!dateString) {
+        dateString = new Date().toISOString();
+    }
+
+    // Check if dateString is a number (timestamp) or a valid date string
+    let date;
+    if (!isNaN(dateString)) {
+        date = new Date(parseInt(dateString));
+    } else {
+        date = new Date(dateString);
+    }
+
+    // Respond with the appropriate JSON
+    if (date.toString() === "Invalid Date") {
+        res.json({ error: "Invalid Date" });
+    } else {
+        res.json({ unix: date.getTime(), utc: date.toUTCString() });
+    }
+});
+
+
+app.listen(8080, () => {
+    console.log('App is listening on port 8080');
+});
--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [dateData, setDateData] = useState(() => {
+    const d = new Date();
+    return `${d.getMonth() + 1}-${d.getDate()}-${d.getFullYear()}`;
+});
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+      <header className="App-header"> 
+        <h1>Welcome to Time Stamp</h1>
+        <p >enter a date </p>
+        <input type='date' value={dateData} onChange={e=>setDateData(e.target.value)}></input>
+        <a href={`http://localhost:8080/api/timestamp/:${dateData}`}>{`http://localhost:8080/api/timestamp/:${dateData}`}</a>
       </header>
     </div>
   );
