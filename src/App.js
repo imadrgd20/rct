f137fd1c609140c5c0553d138461fdce95861754
imadrgd20 <reguadiimad@gmail.com>
2024-05-31 17:41:45 +0100

    TimeStampd

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
+  const baseUrl = 'https://8080-imadrgd20-rct-xzx8dlzkkve.ws-eu114.gitpod.io/api/timestamp/';
+
   const [dateData, setDateData] = useState(() => {
     const d = new Date();
     return `${d.getMonth() + 1}-${d.getDate()}-${d.getFullYear()}`;
-});
+  });
+
+  const timestampUrl = baseUrl + dateData;
 
   return (
     <div className="App">
-      
       <header className="App-header"> 
         <h1>Welcome to Time Stamp</h1>
-        <p >enter a date </p>
-        <input type='date' value={dateData} onChange={e=>setDateData(e.target.value)}></input>
-        <a href={`http://localhost:8080/api/timestamp/:${dateData}`}>{`http://localhost:8080/api/timestamp/:${dateData}`}</a>
+        <p>Enter a date</p>
+        <input type='date' value={dateData} onChange={e => setDateData(e.target.value)}></input>
+        <a href={timestampUrl}>{timestampUrl}</a>
       </header>
     </div>
   );
